46cff95abc48631664f9283a8e072be468fd1b7c
sahilmankar <sahilmankar311@gmail.com>
2023-12-22 22:14:22 +0530

    file creation added from focused document

--- src/extension.ts
+++ src/extension.ts
@@ -1,42 +1,41 @@
+import * as vscode from "vscode";
+import {
+  createFile,
+  getFileContent,
+  getFileName,
+  getFolderPathFromActiveFile,
+  getNamespace,
+  getSelectedTemplate,
+  getTemplateContent,
+} from "./helpers";
 
-import * as vscode from 'vscode';
-import { createFile, getFileContent, getFileName, getNamespace, getSelectedTemplate, getTemplateContent } from "./helpers";
 
-export const extensionChannel = vscode.window.createOutputChannel("csharp-templates");
+export const extensionChannel =
+  vscode.window.createOutputChannel("csharp-templates");
 
 export function activate(context: vscode.ExtensionContext) {
+ 
+  let disposable = vscode.commands.registerCommand(
+    "csharp-templates.addFile",
+    async (currentFolder: vscode.Uri) => {
+      try {
+        if (currentFolder == undefined) {
+          currentFolder = await getFolderPathFromActiveFile();
+        }
+        const namespcenamePromise = getNamespace(currentFolder);
+        const selectedTemplate = await getSelectedTemplate();
+        const fileName = await getFileName();
+        const namespcename = await namespcenamePromise;
+        const templateContent = await getTemplateContent(selectedTemplate);
+        const fileContent = getFileContent(namespcename,fileName,templateContent );
+        await createFile(currentFolder, fileName, fileContent);
+      } catch (error: any) {
+        extensionChannel.appendLine(error);
+      }
+    }
+  );
 
-	
-	console.log('Congratulations, your extension "csharp-templates" is now active!');
-
-	let disposable = vscode.commands.registerCommand(
-		"csharp-templates.addFile",
-		async (currentFolder:vscode.Uri) => {
-	
-		  try {
-		  const fpath = currentFolder.fsPath;
-		 
-		  if (!fpath) {
-			vscode.window.showErrorMessage("Error getting folder path.");
-			throw new Error("Error getting folder path.");
-		  }
-		  const folderPath = currentFolder.fsPath;
-		  // it is started early to not delay while finding namespace
-		  const namespcenamePromise = getNamespace(currentFolder, folderPath);
-		  const selectedTemplate = await getSelectedTemplate();
-		  const fileName = await getFileName();
-		  const namespcename= await namespcenamePromise;
-		  const templateContent = await getTemplateContent(selectedTemplate);
-		  const fileContent = getFileContent(namespcename, fileName, templateContent);
-		  await createFile(folderPath, fileName, fileContent);
-		
-		}catch(error:any){
-		  extensionChannel.appendLine(error);
-		}
-	  });
-	
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
--- src/helpers.ts
+++ src/helpers.ts
@@ -5,13 +5,24 @@ import path from "path";
 import { templates } from "./templates";
 import { extensionChannel } from "./extension";
 
+export async function getFolderPathFromActiveFile(){
+    const activeTextEditor = vscode.window.activeTextEditor;
+			if (activeTextEditor) {
+			  const activeFileUri = activeTextEditor.document.uri;
+        const activeFileDirectoryUri = vscode.Uri.file(path.dirname(activeFileUri.fsPath));
+        return activeFileDirectoryUri;
+      }
+    vscode.window.showErrorMessage("Unable to find folderpath")
+    throw new Error("Unable to find folderpath")
+}
+
 export async function getSelectedTemplate(): Promise<string> {
   const selectedTemplate = await showPicker();
   extensionChannel.appendLine(`Selected Template ${selectedTemplate}`);
   return selectedTemplate;
 }
 
-export async function showPicker() : Promise<string>{
+export async function showPicker(): Promise<string> {
   const quickPickItems: vscode.QuickPickItem[] = [];
   templates.forEach((template) => {
     quickPickItems.push({
@@ -23,7 +34,7 @@ export async function showPicker() : Promise<string>{
   const selectedOption = await vscode.window.showQuickPick(quickPickItems, {
     placeHolder: "Select an option",
     matchOnDescription: true,
-    canPickMany:false
+    canPickMany: false,
   });
 
   if (!selectedOption) {
@@ -46,43 +57,22 @@ export async function getFileName(): Promise<string> {
   return fileName;
 }
 
-export async function getNamespace(file: vscode.Uri,originalFilePath: string): Promise<string | null> {
-  const relativePattern = new vscode.RelativePattern(file, "*.csproj");
-
-  const uris = await vscode.workspace.findFiles(relativePattern);
-
-  if (uris.length > 0) {
-    const filePath = uris[0];
-    // from csproj file -> created file path
-    const originalFilePathInParent = path.relative(filePath.fsPath,originalFilePath);
-    const folderPathForNamespace = originalFilePathInParent
-      .replaceAll("..", "")
-      .replaceAll(".", "")
-      .split(path.sep);
-
-    const projectFileName = path.basename(filePath.fsPath, ".csproj");
-    const namespaceName = projectFileName + folderPathForNamespace.join(".");
-    extensionChannel.appendLine(`Found namespace ${namespaceName}`);
-    return namespaceName;
+export async function getNamespace(folder: vscode.Uri): Promise<string | null> {
+  const file = await getNeighborFilesWithExtension(folder, "cs");
+  if (file != null) {
+    const namespace = await getNamespaceFromfile(file);
+    extensionChannel.appendLine(`namespace found in neighbour file -${namespace}`);
+    return namespace;
   }
+ return buildNamespaceFromPath(folder,folder.fsPath);
 
-  // if not found, search in its parent directory
-  const directory = path.dirname(file.fsPath);
-
-  // Check if the directory is the root directory
-  const isRootDirectory = path.dirname(directory) === directory;
-
-  if (!isRootDirectory) {
-    return getNamespace(vscode.Uri.file(directory), originalFilePath);
-  }
-
-  extensionChannel.appendLine("Error Finding namespace .");
-
-  return null; // Terminate the recursion when the root directory is reached
 }
 
 export function getExtensionUri(): Uri | null {
-  return (vscode.extensions.getExtension("sahilmankar.csharp-templates")?.extensionUri ?? null );
+  return (
+    vscode.extensions.getExtension("sahilmankar.csharp-templates")
+      ?.extensionUri ?? null
+  );
 }
 
 export async function getTemplateContent(
@@ -99,26 +89,109 @@ export async function getTemplateContent(
   return document.getText();
 }
 
-export function getFileContent(namespcename: string | null,fileName: string,Templatecontent: string): string {
+export function getFileContent(
+  namespcename: string | null,
+  fileName: string,
+  Templatecontent: string
+): string {
   let fileContent: string = "";
   if (namespcename) {
-    fileContent += Templatecontent.replace( "namespaceName", namespcename).replaceAll("className", fileName);
+    fileContent += Templatecontent.replace("namespaceName",namespcename).replaceAll("className", fileName);
   } else {
-    fileContent += Templatecontent.replace("namespaceName", "").replaceAll("className",fileName );
+    fileContent += Templatecontent.replace("namespaceName", "").replaceAll("className",fileName);
   }
   return fileContent;
 }
 
-export async function createFile(folderPath: string,fileName: string,content: string): Promise<void> {
-  const filePath = path.join(folderPath, `${fileName}.cs`);
+export async function createFile(
+  folderPath: vscode.Uri,
+  fileName: string,
+  content: string
+): Promise<void> {
+  const filePath = path.join(folderPath.fsPath, `${fileName}.cs`);
   if (fs.existsSync(filePath)) {
     vscode.window.showErrorMessage(`File '${fileName}' already exists.`);
   } else {
     const snippetString = new vscode.SnippetString(content);
-    fs.writeFileSync(filePath,"");
+    fs.writeFileSync(filePath, "");
     const newDocument = await vscode.workspace.openTextDocument(filePath);
     const editor = await vscode.window.showTextDocument(newDocument);
     await editor.insertSnippet(snippetString);
     extensionChannel.appendLine(`File Created Sucessfully `);
   }
 }
+
+export async function getNamespaceFromfile(file: vscode.Uri) {
+  const document = await vscode.workspace.openTextDocument(file);
+
+  const fileContent = document.getText();
+  const lines: string[] = fileContent.split("\n");
+
+  const namespaceLine: string | undefined = lines.find(
+    (line) => line.trim().startsWith("namespace") && line.trim().endsWith(";")
+  );
+
+  if (namespaceLine) {
+    return namespaceLine.replace("namespace", "").replace(";", "").trim();
+  }
+  return null;
+}
+export async function getNeighborFilesWithExtension(
+  directory: vscode.Uri,
+  extension: string
+) {
+  const files = await vscode.workspace.fs.readDirectory(directory);
+
+
+  const filesWithExtension = files.filter(
+    ([filename, type]) =>
+      type === vscode.FileType.File && filename.endsWith(extension)
+  );
+
+  if (!filesWithExtension || filesWithExtension.length < 1) {
+    return null;
+  }
+
+  const [filename] = filesWithExtension[0];
+
+  return vscode.Uri.joinPath(directory, filename);
+}
+
+
+ export async function buildNamespaceFromPath(folder: vscode.Uri,originalFilePath: string): Promise<string | null> {
+
+  const relativePattern = new vscode.RelativePattern(folder, "*.csproj");
+
+  const uris = await vscode.workspace.findFiles(relativePattern);
+
+  if (uris.length > 0) {
+    const filePath = uris[0];
+    // from csproj file -> created file path
+    const originalFilePathInParent = path.relative(
+      filePath.fsPath,
+      originalFilePath
+    );
+    const folderPathForNamespace = originalFilePathInParent
+      .replaceAll("..", "")
+      .replaceAll(".", "")
+      .split(path.sep);
+
+    const projectFileName = path.basename(filePath.fsPath, ".csproj");
+    const namespaceName = projectFileName + folderPathForNamespace.join(".");
+    extensionChannel.appendLine(`Found namespace ${namespaceName}`);
+    return namespaceName;
+  }
+
+  // if not found, search in its parent directory
+  const directory = path.dirname(folder.fsPath);
+
+  // Check if the directory is the root directory
+  const isRootDirectory = path.dirname(directory) === directory;
+
+  if (!isRootDirectory) {
+    return buildNamespaceFromPath(vscode.Uri.file(directory), originalFilePath);
+  }
+  extensionChannel.appendLine("Error Finding namespace .");
+
+  return null; // Terminate the recursion when the root directory is reached
+ }
